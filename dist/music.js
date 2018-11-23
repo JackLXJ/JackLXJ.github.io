228b5c6de6c997b1401c315d7e1d9748d80df98f
txj <510087153@qq.com>
2018-11-23 22:54:54 +0800

    Site updated: 2018-11-23 22:54:53

--- dist/music.js
+++ dist/music.js
@@ -4,22 +4,75 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
+        name: "Butter-Fly",
+        artist: '和田光司',
         url: 'http://cube-1252774894.cosgz.myqcloud.com/music/source/和田光司 - Butter-Fly (ピアノヴァージョン).mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-      },
+        cover: 'http://cube-1252774894.cosgz.myqcloud.com/music/image/和田光司 - Butter-Fly (ピアノヴァージョン).jpg',
+      }，
+
+
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-      },
+        name: "Butter-Fly",
+        artist: '和田光司',
+        url: 'http://www.ytmp3.cn/down/33644.mp3',
+        cover: 'http://img.ytmp3.cn/image/91.jpg',
+      }，
+
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
-      }
+        name: "思念是一种病",
+        artist: '张震岳;蔡健雅',
+        url: 'http://www.ytmp3.cn/down/44304.mp3',
+        cover: 'http://img.ytmp3.cn/image/90.jpg',
+      }，
+      
+      {
+        name: '我的心太乱',
+        artist: '周传雄',
+        url: 'http://www.ytmp3.cn/down/48418.mp3',
+        cover: 'http://img.ytmp3.cn/image/12.jpg',
+      }，
+
+      {
+        name: "黄昏",
+        artist: '周传雄',
+        url: 'http://www.ytmp3.cn/down/33941.mp3',
+        cover: 'http://img.ytmp3.cn/image/29.jpg',
+      }，
+
+       {
+        name: "Butter-Flyy",
+        artist: '梁博 ',
+        url: 'http://www.ytmp3.cn/down/42631.mp3',
+        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2056984611,2592659515&fm=58&bpow=580&bpoh=819',
+      }，
+
+      {
+        name: "男孩",
+        artist: '梁博',
+        url: 'http://www.ytmp3.cn/down/43366.mp3',
+        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2056984611,2592659515&fm=58&bpow=580&bpoh=819',
+      }，
+
+      {
+        name: "B再飞行",
+        artist: '逃跑计划',
+        url: 'http://www.ytmp3.cn/down/48037.mp3',
+        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1913055255,3554545434&fm=58',
+      }，
+
+      {
+        name: "Waiting In The Wings",
+        artist: 'Shayne Ward',
+        url: 'http://www.ytmp3.cn/down/54004.mp3',
+        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=507081678,4248484065&fm=58&w=121&h=140&img.JPEG',
+      }，
+
+      {
+        name: "Apologize",
+        artist: 'OneRepublic',
+        url: 'http://www.ytmp3.cn/down/55044.mp3',
+        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=920017221,1532534424&fm=58&w=121&h=140&img.JPEG',
+      }，
+      
     ]
 });
