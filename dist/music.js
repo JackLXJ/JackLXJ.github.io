0d6132317e2549eb1c0227fe99ee55a1cf29d7bc
txj <510087153@qq.com>
2018-11-23 23:00:50 +0800

    Site updated: 2018-11-23 23:00:50

--- dist/music.js
+++ dist/music.js
@@ -32,47 +32,7 @@ const ap = new APlayer({
         cover: 'http://img.ytmp3.cn/image/12.jpg',
       }，
 
-      {
-        name: "黄昏",
-        artist: '周传雄',
-        url: 'http://www.ytmp3.cn/down/33941.mp3',
-        cover: 'http://img.ytmp3.cn/image/29.jpg',
-      }，
-
-       {
-        name: "Butter-Flyy",
-        artist: '梁博 ',
-        url: 'http://www.ytmp3.cn/down/42631.mp3',
-        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2056984611,2592659515&fm=58&bpow=580&bpoh=819',
-      }，
-
-      {
-        name: "男孩",
-        artist: '梁博',
-        url: 'http://www.ytmp3.cn/down/43366.mp3',
-        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2056984611,2592659515&fm=58&bpow=580&bpoh=819',
-      }，
-
-      {
-        name: "B再飞行",
-        artist: '逃跑计划',
-        url: 'http://www.ytmp3.cn/down/48037.mp3',
-        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1913055255,3554545434&fm=58',
-      }，
-
-      {
-        name: "Waiting In The Wings",
-        artist: 'Shayne Ward',
-        url: 'http://www.ytmp3.cn/down/54004.mp3',
-        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=507081678,4248484065&fm=58&w=121&h=140&img.JPEG',
-      }，
-
-      {
-        name: "Apologize",
-        artist: 'OneRepublic',
-        url: 'http://www.ytmp3.cn/down/55044.mp3',
-        cover: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=920017221,1532534424&fm=58&w=121&h=140&img.JPEG',
-      }，
+      
       
     ]
 });
